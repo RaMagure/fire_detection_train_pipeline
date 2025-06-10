2e9958f86fadfd3b92c63b684d144bfd3680d7c7
RaMagure <rahul.m@magureinc.com>
2025-06-10 11:39:26 +0530

    update the url

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pipeline.traning_pipeline import TrainModel
 
 def main():
-    TrainModel(model_path="models/firedetect-11s.pt",batch_size=64,data_path_url="https://drive.google.com/file/d/1yM4ududikzC2PYyeUIZP4TypgGzwSF2z/view?usp=drive_link").train()
+    TrainModel(model_path="models/firedetect-11s.pt",batch_size=64,data_path_url="https://drive.google.com/drive/folders/10k5qeGudeWV7_gJHQvQWHFu-whnOM8PV?usp=drive_link").train()
     pass
 
 
